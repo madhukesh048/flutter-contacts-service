4d109bccfe6d2a8a47403e1ba22e448225c84f76
Madhukesh D <madhukesh04@gmail.com>
2021-01-05 11:35:56 +0530

    inital commit

---
+++ lib/homepage.dart
@@ -0,0 +1,57 @@
+import 'package:contacts_service/contacts_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Iterable<Contact> _contacts;
+
+  @override
+  void initState() {
+    getContacts();
+    super.initState();
+  }
+
+  Future<void> getContacts() async {
+    final Iterable<Contact> contacts = await ContactsService.getContacts(
+      withThumbnails: false,
+    );
+    setState(() {
+      _contacts = contacts;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contacts"),
+      ),
+      body: _contacts != null
+          ? ListView.builder(
+              itemCount: _contacts?.length ?? 0,
+              itemBuilder: (BuildContext context, int index) {
+                Contact contact = _contacts?.elementAt(index);
+                return ListTile(
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 2, horizontal: 18),
+                  leading: (contact.avatar != null && contact.avatar.isNotEmpty)
+                      ? CircleAvatar(
+                          backgroundImage: MemoryImage(contact.avatar),
+                        )
+                      : CircleAvatar(
+                          child: Text(contact.initials()),
+                          backgroundColor: Theme.of(context).accentColor,
+                        ),
+                  title: Text(contact.displayName ?? ''),
+                );
+              },
+            )
+          : Center(child: const CircularProgressIndicator()),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:contacts_app/homepage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PermissionButton(),
+    );
+  }
+}
+
+class PermissionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            onPressed: () async {
+              final PermissionStatus permissionStatus = await _getPermission();
+              if (permissionStatus == PermissionStatus.granted) {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomePage()));
+              } else {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) => CupertinoAlertDialog(
+                    title: Text('Permissions error'),
+                    content: Text('Please enable contacts access '
+                        'permission in system settings'),
+                    actions: <Widget>[
+                      CupertinoDialogAction(
+                        child: Text('OK'),
+                        onPressed: () => Navigator.of(context).pop(),
+                      )
+                    ],
+                  ),
+                );
+              }
+            },
+            child: Container(child: Text('See Contacts')),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<PermissionStatus> _getPermission() async {
+    final PermissionStatus permission = await Permission.contacts.status;
+    if (permission != PermissionStatus.granted &&
+        permission != PermissionStatus.denied) {
+      final Map<Permission, PermissionStatus> permissionStatus =
+          await [Permission.contacts].request();
+      return permissionStatus[Permission.contacts] ??
+          PermissionStatus.undetermined;
+    } else {
+      return permission;
+    }
+  }
+}
